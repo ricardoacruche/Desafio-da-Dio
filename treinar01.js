62195f7b403b010330a125dc005b0cc035bdfba8
ricardoacruche <richardbusiness29@aol.com>
2024-09-26 11:07:42 -0300

    Add files via upload

---
+++ treinar01.js
@@ -0,0 +1,9 @@
+let possuiOvos = false
+let itensComprados = ""
+
+if (possuiOvos) {itensComprados = "Leite"}
+
+else{console.log ("passou na sessao de congelados")
+    itensComprados = "Lasanha congelada"}
+
+console.log ("item comprado: " + itensComprados)
